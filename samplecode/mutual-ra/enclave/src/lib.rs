bb722448edd463fa9401c1cb2cda0e477cf68d20
jiangyizhe <1239865849@qq.com>
2022-07-21 13:49:07 +0800

    fix:mutual-ra update v4 version

--- samplecode/mutual-ra/enclave/src/lib.rs
+++ samplecode/mutual-ra/enclave/src/lib.rs
@@ -65,8 +65,8 @@ mod cert;
 mod hex;
 
 pub const DEV_HOSTNAME:&'static str = "api.trustedservices.intel.com";
-pub const SIGRL_SUFFIX:&'static str = "/sgx/dev/attestation/v3/sigrl/";
-pub const REPORT_SUFFIX:&'static str = "/sgx/dev/attestation/v3/report";
+pub const SIGRL_SUFFIX:&'static str = "/sgx/dev/attestation/v4/sigrl/";
+pub const REPORT_SUFFIX:&'static str = "/sgx/dev/attestation/v4/report";
 pub const CERTEXPIRYDAYS: i64 = 90i64;
 
 extern "C" {
