3d3f60dbd091d0fc45029afb04250f9b153d0d35
jiangyizhe <1239865849@qq.com>
2022-08-25 17:33:41 +0800

    test

--- samplecode/ue-ra/ue-ra-server/enclave/src/lib.rs
+++ samplecode/ue-ra/ue-ra-server/enclave/src/lib.rs
@@ -525,7 +525,7 @@ pub extern "C" fn run_server(socket_fd : c_int, sign_type: sgx_quote_sign_type_t
 
     // let root_ca_bin = include_bytes!("../../../cert/ca.crt");
     // let mut ca_reader = BufReader::new(&root_ca_bin[..]);
-    // let mut rc_store = rustls::RootCertStore::empty();
+    let mut rc_store = rustls::RootCertStore::empty();
     // // Build a root ca storage
     // rc_store.add_pem_file(&mut ca_reader).unwrap();
     // // Build a default authenticator which allow every authenticated client
